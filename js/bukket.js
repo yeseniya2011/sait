4fe51ad626a755b1c5bc8aaa0f57487bd96a18ec
yeseniya2011 <eseniavagner@gmail.com>
2022-06-24 16:03:36 +0300

    kika

--- js/bukket.js
+++ js/bukket.js
@@ -49,3 +49,97 @@ create(
     "./img/krasota/sirnik/cheese.PNG",
     "sb-365"
 );
+
+create(
+    "Свежий Фруктовый роликовый бальзам для губ",
+    274,
+    "osveshitel.html",
+    "./img/krasota/osveshitel/osveshitel.PNG",
+    "365-byr"
+);
+
+create(
+    "12 видов цветов Подводка для глаз жидкая Водонепроницаемая",
+    253,
+    "podvodka.html",
+    "./img/krasota/podvodka/podvodka.PNG",
+    "xbd-74754"
+);
+
+create(
+    "Миниатюрный Жидкий блеск для губ",
+    365,
+    "muhtar.html",
+    "./img/krasota/pomada/yaUstaalaaaaaa.PNG",
+    "g647m"
+);
+
+create(
+    "Kawaii Sanriod аниме периферическая Mymelody Kuromi Kitty Cinnamoroll",
+    461,
+    "palochki.html",
+    "./img/krasota/palochki/palochki.PNG",
+    "366-Tvxy"
+);
+
+create(
+    "Kawaii Подлинная Корица собака с большим ухом Baby Bottle",
+    226,
+    "malo.html",
+    "./img/krasota/malo/raznaa.PNG",
+    "47fbf-gsuv"
+);
+
+create(
+    "спонж для макияжа, сухой и влажный спонж",
+    153,
+    "sponshik.html",
+    "./img/krasota/sponsh/sponsh.PNG",
+    "4fr6red"
+);
+
+create(
+    "1 шт. кисти для макияжа Кошачий коготь Shap",
+    184,
+    "kist.html",
+    "./img/krasota/kist/lapka.PNG",
+    "7d-dhede"
+);
+
+create(
+    "Sendo Крем для тела Лимонный пирог, 200 мл",
+    285,
+    "krem.html",
+    "./img/krasota/sendo/sendo.PNG",
+    "47bdyceef"
+);
+
+create(
+    "Увлажнитель-Ароматизатор воздуха",
+    1670,
+    "uvlashnitel.html",
+    "./img/deti/zaika/zaika.PNG",
+    "375-ncscfVDw"
+);
+
+create(
+    "Настольный органайзер W & G Kawaii",
+    2598,
+    "ohanaicer.html",
+    "./img/deti/organaizer/organaizer.PNG",
+    "xsbyceYce-47756"
+);
+
+create(
+    "Коробка для салфеток под телевизор",
+    375,
+    "televisorik.html",
+    "./img/deti/televisorika/televisor.PNG",
+    "375sbve"
+);
+
+create(
+    "Настольный органайзер W & G",
+    3000,
+    
+)
